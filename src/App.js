405019a4843f38de74957d71a4eeb9ac2cf74871
aleiton <alejandro.leiton@meraki.net>
2021-10-13 20:11:44 -0300

    Inital commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,43 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <header>VERDULERIA</header>
+      <div className="list">
+        <h1>Carrito:</h1>
+        <div className="list-item">
+          <span>Bananas 🍌</span>
+          <div className="list-item-info">
+            <input value={4} />
+            <span>$15 c/u</span>
+          </div>
+        </div>
+        <div className="list-item">
+          <span>Manzanas 🍎</span>
+          <div className="list-item-info">
+            <input value={6} />
+            <span>$10 c/u</span>
+          </div>
+        </div>
+        <div className="list-item">
+          <span>Sandia 🍉</span>
+          <div className="list-item-info">
+            <input value={1} />
+            <span>$60 c/u</span>
+          </div>
+        </div>
+        <div className="list-item">
+          <span>Kiwi 🥝</span>
+          <div className="list-item-info">
+            <input value={5} />
+            <span>$20 c/u</span>
+          </div>
+        </div>
+        <div className="total-info">
+            <span>16</span>
+            <span>$280</span>
+        </div>
+      </div>
     </div>
   );
 }
